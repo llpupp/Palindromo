b0cfade502391922d2bdc116525def99dd9d092b
llpupp <llpupp@hotmail.com>
2022-12-01 09:54:09 -0300

    Inclusão do Readme

---
+++ README.md
@@ -0,0 +1,2 @@
+**IDENTIFICAÇÃO DE UM PALÍNDROMO**
+<br/>Palíndromo se traduz numa palavra que, quando escrito ao contrário, é exatamente igual à palavra original 
--- src/Palindromo.java
+++ src/Palindromo.java
@@ -1,5 +1,24 @@
-public class Main {
+import java.util.Scanner;
+
+public class Palindromo {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner scan = new Scanner(System.in);
+
+        String palavra = scan.nextLine();
+
+        palindromo(palavra);
+
+    }
+
+    private static void palindromo(String palavra) {
+        String palavraInvertida = new StringBuffer(palavra).reverse().toString();
+        if (palavra.equals(palavraInvertida)) {
+
+            System.out.println("TRUE");
+        } else {
+
+            System.out.println("FALSE");
+        }
     }
 }
